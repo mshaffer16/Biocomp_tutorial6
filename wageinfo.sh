63d05b2ea156e88de9c0dd6046463f8f919405a4
Marlee Shaffer <mshaffe2@nd.edu>
2021-10-04 10:39:30 -0400

    Adds code for highest earner info

--- wageinfo.sh
+++ wageinfo.sh
@@ -3,3 +3,5 @@
 
 cat wages.csv | tail -n 3294 | cut -d , -f 1,2 | tr "," " " | sort -k1,1 -k2,2n | uniq > uniqgend-yearsExp.txt
 
+#This section returns the gender, years experience, and wage for the highest earner
+cat "$1" | cut -d , -f 1,2,4 | tr "," " " | sort -k3rn | head -n 1 
